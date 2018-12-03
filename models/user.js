497c404c950965aaab5c9935e9c39bc68439fd7d
sovovy <sovovy@gmail.com>
2018-12-03 18:49:56 +0900

    계정에 admin 구분 key 추가

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const LoginSchema = new mongoose.Schema({
   id: String,
   pwd: String,
+  admin: Boolean
 });
 
 module.exports = mongoose.model('user', LoginSchema);
